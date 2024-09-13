ad42c2dfdde0b7444200da6ebc26a54858dc67a6
Reginaldo Gomes <reginaldo@bhsolutions.com.br>
2024-09-12 23:00:21 -0300

    new: roteamento ajax, paginas, objetos de dados e estrutura do projeto

---
+++ js/data/menu.js
@@ -0,0 +1,8 @@
+// Objeto com os itens do menu
+const menuItems = [
+  { name: 'Home', link: '/' },
+  { name: 'Sobre', link: '/about' },
+  { name: 'Serviços', link: '/services' },
+  { name: 'Contato', link: '/contact' },
+  { name: 'Blog', link: '/blog' }
+];
