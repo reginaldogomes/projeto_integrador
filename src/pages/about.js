507af039b4b50d239d59ee49ba790ebe01568fe4
Reginaldo Gomes <reginaldo@bhsolutions.com.br>
2024-09-20 23:34:28 -0300

    new: blog css, cards html

--- src/pages/about.js
+++ src/pages/about.js
@@ -6,7 +6,9 @@ export function renderAbout() {
           <h1>Sobre o Floripa Tour</h1>
           <h3>Conheça nossa historia</h3>
         </div>
-        <p>Bem-vindo ao Portal de Turismo de Florianópolis, seu guia definitivo para explorar os encantos naturais e culturais desta ilha paradisíaca. Com o objetivo de oferecer uma experiência imersiva e informativa, nosso portal destaca os principais pontos turísticos de Florianópolis, desde suas praias mundialmente famosas até as áreas de preservação ambiental que fazem da cidade um destino único para o ecoturismo.</p>
+        <p>Bem-vindo ao Portal de Turismo de Florianópolis, seu guia definitivo para explorar os encantos naturais e culturais desta ilha paradisíaca.
+        Com o objetivo de oferecer uma experiência imersiva e informativa, nosso portal destaca os principais pontos turísticos de Florianópolis, desde
+        suas praias mundialmente famosas até as áreas de preservação ambiental que fazem da cidade um destino único para o ecoturismo.</p>
       </section>
   `;
 }
