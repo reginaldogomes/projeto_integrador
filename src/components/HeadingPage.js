21899a9fc18d15c98531bd679d08b3d4e9502e60
Reginaldo Gomes <reginaldo@bhsolutions.com.br>
2024-09-28 00:42:17 -0300

    novo componente heading page

---
+++ src/components/HeadingPage.js
@@ -0,0 +1,20 @@
+// src/HeadingPage.js
+export function HeadingPage({ title, subtitle }) {
+  // Cria o elemento principal
+  const headingPage = document.createElement('div')
+  headingPage.className = 'heading-page'
+
+  // Cria o título
+  const h1 = document.createElement('h1')
+  h1.textContent = title
+
+  // Cria o subtítulo
+  const h3 = document.createElement('h3')
+  h3.textContent = subtitle
+
+  // Anexa os elementos
+  headingPage.appendChild(h1)
+  headingPage.appendChild(h3)
+
+  return headingPage
+}
--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,49 +1,62 @@
-import { posts } from '../data/posts'
-
+import { posts } from '@/data/posts'
+import { HeadingPage } from '@/components/HeadingPage'
 import '@/assets/css/blog.css'
 
+// Render Blog function
 export function renderBlog() {
   const app = document.getElementById('app')
-  const section = document.createElement('section')
-  section.className = 'blog-section'
 
-  const headingPage = document.createElement('div')
-  headingPage.className = 'heading-page'
-
-  const title = document.createElement('h1')
-  title.textContent = 'Blog'
-  const subtitle = document.createElement('h3')
-  subtitle.textContent = 'Noticias sobre ecoturismo em Floripa'
-
-  headingPage.appendChild(title)
-  headingPage.appendChild(subtitle)
-  section.appendChild(headingPage)
-
-  // Cria o container fora do forEach
-  const postElement = document.createElement('div')
-  postElement.className = 'container'
-
-  posts.forEach((post) => {
-    // Cria cada artigo individualmente
-    const article = document.createElement('article')
-    article.innerHTML = `
-      <img src="${post.urlImg}" alt="Imagem do Post 1">
-      <header>
-        <h2>${post.title}</h2>
-      </header>
-      <p>${post.content}</p>
-      <p><strong>Autor:</strong> ${post.author}</p>
-      <footer>
-        <a href="#" class="read-more">Leia mais</a>
-      </footer>
-    `
-
-    // Adiciona o artigo ao container
-    postElement.appendChild(article)
+  // Criação da seção e seus elementos
+  const section = createSection()
+  const headingPage = HeadingPage({
+    title: 'Blog',
+    subtitle: 'Noticias sobre ecoturismo em Floripa',
   })
 
-  // Adiciona o container ao section
-  section.appendChild(postElement)
+  const postContainer = createPostContainer(posts)
 
+  // Utilização de DocumentFragment para evitar múltiplas inserções no DOM
+  const fragment = document.createDocumentFragment()
+  fragment.appendChild(headingPage)
+  fragment.appendChild(postContainer)
+
+  section.appendChild(fragment)
   app.appendChild(section)
+}
+
+function createSection() {
+  const section = document.createElement('section')
+  section.className = 'blog-section'
+  return section
+}
+
+function createPostContainer(posts) {
+  const postContainer = document.createElement('div')
+  postContainer.className = 'container'
+
+  posts.forEach((post) => {
+    const article = createPostElement(post)
+    postContainer.appendChild(article)
+  })
+
+  return postContainer
+}
+
+// Render Blog function
+function createPostElement(post) {
+  const article = document.createElement('article')
+  article.innerHTML = `
+    <figure>
+      <img src="${post.urlImg}" alt="Imagem do Post">
+    </figure>
+    <header>
+      <h2>${post.title}</h2>
+    </header>
+    <p>${post.content}</p>
+    <p><strong>Autor:</strong> ${post.author}</p>
+    <footer>
+      <a href="#" class="read-more">Leia mais</a>
+    </footer>
+  `
+  return article
 }
