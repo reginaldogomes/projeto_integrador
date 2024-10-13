683dcc17f232c8248bba1cf27fb19d842fe6d4cc
VictorDanielSilva <victordanielbatista.contato@gmail.com>
2024-10-13 12:10:42 -0300

    Adiconando e Formatando Código

--- src/pages/tours.js
+++ src/pages/tours.js
@@ -42,3 +42,35 @@ export function renderTours() {
     cardsContainer.appendChild(card)
   })
 }
+
+function createCard({ imgSrc, imgAlt, title, description }) {
+  const card = document.createElement('div')
+  card.className = 'card'
+
+  const cardHeader = document.createElement('div')
+  cardHeader.className = 'card-header'
+  const img = document.createElement('img')
+  img.src = imgSrc
+  img.alt = imgAlt
+  cardHeader.appendChild(img)
+
+  const cardBody = document.createElement('div')
+  cardBody.className = 'card-body'
+  const cardTitle = document.createElement('h3')
+  cardTitle.textContent = title
+  const cardDescription = document.createElement('p')
+  cardDescription.textContent = description
+  const cardButton = document.createElement('a')
+  cardButton.href = '#'
+  cardButton.className = 'btn'
+  cardButton.textContent = 'Saiba Mais'
+
+  cardBody.appendChild(cardTitle)
+  cardBody.appendChild(cardDescription)
+  cardBody.appendChild(cardButton)
+
+  card.appendChild(cardHeader)
+  card.appendChild(cardBody)
+
+  return card
+}
