a18e8897e412a8315c7f44d257583b8053145439
Evandro Brandão <evandrobrandao@hotmail.com>
2024-10-15 10:18:44 -0300

    identação do meu css

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -37,8 +37,7 @@ export function renderContact() {
             <button class="button_contact" type="submit">Enviar</button>
           </div>
            <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d15002.503510486065!2d-43.93413985!3d-19.940164399999997!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sbr!4v1728996655863!5m2!1sen!2sbr" width="600" height="350" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
-          
-                   
+                             
         </form>
       </section>
   `
