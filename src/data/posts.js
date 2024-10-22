0d830e0c8c2c0d9fedd2b0f644f8265b3ef5820c
Reginaldo Gomes <reginaldo@bhsolutions.com.br>
2024-10-22 18:12:51 -0300

    update: code refact struture

--- src/data/posts.js
+++ src/data/posts.js
@@ -1,67 +1,67 @@
-// blog.js
 export const posts = [
   {
     id: 1,
-    title: 'Introdução ao ViteJS',
+    title: 'Descubra o Costão do Santinho: História e Natureza em Harmonia',
     excerpt:
-      'ViteJS é uma ferramenta de bundling e desenvolvimento super rápida para projetos front-end.',
+      'Explore o Costão do Santinho, um dos destinos mais icônicos de Florianópolis, que combina belezas naturais com história e arte rupestre.',
     content:
-      'ViteJS é uma ferramenta de bundling e desenvolvimento super rápida para projetos front-end. ViteJS é uma ferramenta de bundling e desenvolvimento super rápida para projetos front-end. ViteJS é uma ferramenta de bundling e desenvolvimento super rápida para projetos front-end.',
+      'Localizado no extremo norte da ilha de Santa Catarina, o Costão do Santinho é conhecido por suas praias paradisíacas e seu importante legado histórico. Uma das maiores atrações são as inscrições rupestres, que datam de mais de cinco mil anos, revelando vestígios de antigas civilizações que habitaram a região. Além de ser um ponto turístico de grande valor histórico, o Costão do Santinho oferece trilhas ecológicas que proporcionam vistas incríveis da flora e fauna local. O resort de luxo que leva o nome da praia é outro atrativo, oferecendo acomodações de primeira classe e uma gama de atividades para toda a família. Se você busca um lugar para relaxar, curtir a natureza e aprender um pouco mais sobre a história local, o Costão do Santinho é o destino perfeito.',
     author: 'João Silva',
     urlImg:
       'https://floripamanha.org/wp-content/uploads/2024/04/costao-santinho-800x533.jpg',
   },
   {
     id: 2,
-    title: 'Por que usar Vanilla JavaScript?',
+    title: 'Lagoa da Conceição: A Vida Noturna e o Coração Cultural de Floripa',
     excerpt:
-      'Vanilla JavaScript oferece uma base leve e eficiente sem a necessidade de frameworks pesados.',
+      'A Lagoa da Conceição é o ponto de encontro perfeito para quem busca agito e cultura, com uma mistura de restaurantes, bares e vida noturna vibrante.',
     content:
-      'Vanilla JavaScript oferece uma base leve e eficiente sem a necessidade de frameworks pesados. Vanilla JavaScript oferece uma base leve e eficiente sem a necessidade de frameworks pesados. Vanilla JavaScript oferece uma base leve e eficiente sem a necessidade de frameworks pesados.',
+      'Situada no centro geográfico da ilha, a Lagoa da Conceição é um dos destinos mais movimentados de Florianópolis. Além de ser cercada por montanhas e florestas exuberantes, a Lagoa é o epicentro da vida noturna da cidade, atraindo turistas e moradores com sua vibrante oferta de bares, restaurantes e casas noturnas. Durante o dia, é possível praticar esportes aquáticos como windsurf e stand up paddle, ou simplesmente relaxar nas margens da lagoa. À noite, o lugar se transforma em um verdadeiro polo cultural, com música ao vivo, festas e uma gastronomia diversificada, que vai desde a culinária local até pratos internacionais. Não deixe de experimentar os frutos do mar frescos em um dos muitos restaurantes ao redor da Lagoa. Com opções para todos os gostos, a Lagoa da Conceição é o lugar ideal para quem quer aproveitar o melhor de Florianópolis, tanto de dia quanto de noite.',
     author: 'Maria Oliveira',
     urlImg:
       'https://floripamanha.org/wp-content/uploads/2024/04/costao-santinho-800x533.jpg',
   },
   {
     id: 3,
-    title: 'Aprendendo Vite com Vanilla JS',
+    title: 'As Melhores Trilhas de Florianópolis para Aventureiros',
     excerpt:
-      'Vanilla JavaScript oferece uma base leve e eficiente sem a necessidade de frameworks pesados.',
+      'Florianópolis é o paraíso dos aventureiros, com trilhas que vão desde passeios tranquilos até desafios para os mais experientes.',
     content:
-      'Combinar Vite com JavaScript puro permite um desenvolvimento moderno com simplicidade. Combinar Vite com JavaScript puro permite um desenvolvimento moderno com simplicidade. Combinar Vite com JavaScript puro permite um desenvolvimento moderno com simplicidade.',
+      'Para os amantes de aventura e natureza, Florianópolis oferece uma variedade de trilhas que atravessam paisagens deslumbrantes e preservadas. Entre as mais famosas, a Trilha da Lagoinha do Leste é um verdadeiro desafio, recompensado pela chegada a uma praia selvagem de beleza intocada. Para os que preferem uma trilha mais leve, a Trilha do Gravatá oferece vistas panorâmicas do mar e das montanhas da ilha. Além disso, a Trilha do Morro da Aranha é ideal para quem busca um pouco de história, pois passa por antigas fortificações e mirantes estratégicos usados na defesa da ilha. Independentemente do nível de dificuldade, todas as trilhas oferecem uma conexão única com a natureza exuberante de Florianópolis, onde o visitante pode observar uma rica biodiversidade e paisagens cinematográficas. Não esqueça de levar água, protetor solar e uma câmera para registrar as paisagens inesquecíveis.',
     author: 'Carlos Pereira',
     urlImg:
       'https://floripamanha.org/wp-content/uploads/2024/04/costao-santinho-800x533.jpg',
   },
   {
     id: 4,
-    title: 'Aprendendo Vite com Vanilla JS',
+    title: 'Jurerê Internacional: Luxo e Diversão em Florianópolis',
     excerpt:
-      'Vanilla JavaScript oferece uma base leve e eficiente sem a necessidade de frameworks pesados.',
+      'Conheça Jurerê Internacional, uma das praias mais badaladas do Brasil, onde o luxo encontra a diversão em Florianópolis.',
     content:
-      'Combinar Vite com JavaScript puro permite um desenvolvimento moderno com simplicidade. Combinar Vite com JavaScript puro permite um desenvolvimento moderno com simplicidade. Combinar Vite com JavaScript puro permite um desenvolvimento moderno com simplicidade.',
-    author: 'Carlos Pereira',
+      'Jurerê Internacional é sinônimo de luxo e sofisticação. Considerada uma das praias mais exclusivas do Brasil, o local oferece uma infraestrutura de alto nível, com resorts, beach clubs e restaurantes requintados. Frequentada por celebridades e turistas exigentes, Jurerê é conhecida por suas festas glamorosas à beira-mar e por seus eventos que atraem pessoas de todo o mundo. Mas a praia não é só para quem busca luxo; suas águas calmas e cristalinas são perfeitas para famílias com crianças ou para aqueles que preferem atividades aquáticas tranquilas, como stand up paddle e caiaque. Além disso, o bairro conta com uma arquitetura moderna e planejada, proporcionando conforto e beleza aos visitantes. Seja para relaxar em um resort de luxo ou para curtir uma festa inesquecível em um dos beach clubs, Jurerê Internacional é o destino certo para quem quer aproveitar o melhor da vida em Florianópolis.',
+    author: 'João Silva',
     urlImg:
       'https://floripamanha.org/wp-content/uploads/2024/04/costao-santinho-800x533.jpg',
   },
   {
     id: 5,
-    title: 'Aprendendo Vite com Vanilla JS',
+    title: 'Praia da Joaquina: O Paraíso dos Surfistas',
     excerpt:
-      'Vanilla JavaScript oferece uma base leve e eficiente sem a necessidade de frameworks pesados.',
+      'A Praia da Joaquina é o destino preferido dos surfistas em Florianópolis, com ondas perfeitas e campeonatos internacionais.',
     content:
-      'Combinar Vite com JavaScript puro permite um desenvolvimento moderno com simplicidade. Combinar Vite com JavaScript puro permite um desenvolvimento moderno com simplicidade. Combinar Vite com JavaScript puro permite um desenvolvimento moderno com simplicidade.',
-    author: 'Carlos Pereira',
+      'Se você é fã de surfe ou simplesmente gosta de ver o esporte em ação, a Praia da Joaquina é o lugar certo para visitar em Florianópolis. Reconhecida internacionalmente por suas ondas fortes e desafiadoras, Joaquina é o palco de grandes campeonatos de surfe que atraem atletas de todo o mundo. Além de ser um paraíso para os surfistas, a praia também oferece opções para quem quer se aventurar nas dunas de areia branca, praticando sandboard ou caminhadas. A infraestrutura local é excelente, com quiosques, restaurantes e lojas que atendem tanto os turistas quanto os praticantes de surfe. A Praia da Joaquina oferece um equilíbrio perfeito entre esportes, natureza e lazer, sendo um dos destinos mais vibrantes e emocionantes de Florianópolis. Não importa se você é iniciante ou profissional no surfe, Joaquina promete momentos inesquecíveis sobre as ondas.',
+    author: 'Maria Oliveira',
     urlImg:
       'https://floripamanha.org/wp-content/uploads/2024/04/costao-santinho-800x533.jpg',
   },
   {
-    id: 5,
-    title: 'Aprendendo Vite com Vanilla JS',
+    id: 6,
+    title:
+      'Ribeirão da Ilha: Um Passeio pela Tradição Açoriana em Florianópolis',
     excerpt:
-      'Vanilla JavaScript oferece uma base leve e eficiente sem a necessidade de frameworks pesados.',
+      'Conheça o charme de Ribeirão da Ilha, um dos bairros mais antigos de Florianópolis, famoso por sua arquitetura açoriana e gastronomia.',
     content:
-      'Combinar Vite com JavaScript puro permite um desenvolvimento moderno com simplicidade. Combinar Vite com JavaScript puro permite um desenvolvimento moderno com simplicidade. Combinar Vite com JavaScript puro permite um desenvolvimento moderno com simplicidade.',
+      'Ribeirão da Ilha é uma verdadeira viagem no tempo. Com suas ruas estreitas e casinhas coloridas de arquitetura açoriana, o bairro mantém viva a tradição dos primeiros colonizadores portugueses que chegaram à ilha. Conhecido pela produção de ostras, o local é um dos maiores exportadores desse fruto do mar no Brasil, e muitos restaurantes oferecem ostras frescas preparadas de diversas maneiras. Além da gastronomia de alta qualidade, Ribeirão da Ilha oferece aos visitantes a chance de explorar o centro histórico, com igrejas e construções que datam do século XVIII. As paisagens bucólicas e o clima tranquilo fazem de Ribeirão um ótimo destino para quem quer escapar do agito da cidade e mergulhar na cultura açoriana. Se você é um amante da boa comida e história, este é um lugar que não pode faltar no seu roteiro em Florianópolis.',
     author: 'Carlos Pereira',
     urlImg:
       'https://floripamanha.org/wp-content/uploads/2024/04/costao-santinho-800x533.jpg',
