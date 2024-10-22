06416784ad1078b3f0d30adbaa314550ae71971c
Reginaldo Gomes <reginaldo@bhsolutions.com.br>
2024-10-22 00:47:25 -0300

    update: config file removed

--- src/data/config.js
+++
@@ -1,9 +0,0 @@
-const config = {
-  // Informações gerais do projeto
-  projectName: 'Floripa Tour',
-  version: '1.0.0',
-}
-
-document.addEventListener('DOMContentLoaded', () => {
-  document.title = config.projectName + ' - ' + config.version
-})
