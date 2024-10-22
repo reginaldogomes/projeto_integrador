7408a3da3f62897cdfceff45149121faddc6ba52
Reginaldo Gomes <reginaldo@bhsolutions.com.br>
2024-10-22 18:22:41 -0300

    update: links images tours

--- src/data/touristSpots.js
+++ src/data/touristSpots.js
@@ -3,7 +3,8 @@
 
 export const touristSpots = [
   {
-    imgSrc: '/assets/img/Passeios/Bombinhas02.jpg',
+    imgSrc:
+      'https://cdn-clubecandeias.s3.sa-east-1.amazonaws.com/uploads/2022/11/bombinhas-santa-catarina-clube-candeias.jpeg',
     imgAlt: 'Bombinhas',
     title: 'Bombinhas',
     description:
@@ -11,35 +12,38 @@ export const touristSpots = [
   },
   {
     imgSrc:
-      'https://viajantesemfim.com.br/wp-content/uploads/2023/01/IMG_3131_jpg-scaled.jpg',
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ89EmsFkyZrRfzoYIdVI2kLyKeLtPze0l8qg&s',
     imgAlt: 'Dunas do Santinho',
     title: 'Dunas do Santinho',
     description:
       'Explore as impressionantes Dunas do Santinho em Florianópolis! Com suas areias brancas e cenários deslumbrantes, este é o refúgio ideal para quem aprecia natureza e aventura.',
   },
   {
-    imgSrc: '/assets/img/Passeios/IlhadoCampeche.jpg',
+    imgSrc:
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSE0PKqemXaoPL4cJCu_Uts7UGfiOWRw2dUow&s',
     imgAlt: 'Ilha do Campeche',
     title: 'Ilha do Campeche',
     description:
       'O passeio de escuna parte da Barra da Lagoa, na região leste da Ilha. Ao longo da navegação, desfrutaremos de vistas panorâmicas deslumbrantes, passando pela prainha da Barra da Lagoa, piscinas naturais.',
   },
   {
-    imgSrc: '/assets/img/Passeios/SulIlha.png',
+    imgSrc:
+      'https://www.carpemundi.com.br/wp-content/uploads/2017/04/campeche-floripa.jpg',
     imgAlt: 'Sul da Ilha',
     title: 'Tour no Sul da Ilha',
     description:
       'Neste passeio, exploraremos a região Sul da Ilha, conhecida por suas praias exuberantes e uma lagoa de água doce.',
   },
   {
-    imgSrc: '/assets/img/Passeios/FortalezadeAnhatomirim.png',
+    imgSrc: 'https://likefloripa.com/wp-content/uploads/2023/01/IMG_7965.jpg',
     imgAlt: 'Fortaleza de Anhatomirim',
     title: 'Fortaleza de Anhatomirim',
     description:
       'Conheça a imponente Fortaleza de Anhatomirim em Florianópolis! Localizada na Ilha de Anhatomirim, esta fortaleza histórica do século XVIII oferece uma verdadeira imersão no passado.',
   },
   {
-    imgSrc: '/assets/img/Passeios/picodacoroa.png',
+    imgSrc:
+      'https://i.pinimg.com/736x/1d/14/d2/1d14d2599f38ea6df9383c4cd2e4a652.jpg',
     imgAlt: 'Pico da Coroa',
     title: 'Trilha Pico da Coroa',
     description:
